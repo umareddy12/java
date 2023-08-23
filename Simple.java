d2cc3cd18e426842608d292dde8ee29054b2f445
Reddy Uma Devi <121800119+umareddy12@users.noreply.github.com>
2023-08-23 18:24:51 +0530

    Add files via upload

---
+++ Simple.java
@@ -0,0 +1,9 @@
+//hello world program
+public class Simple //class name
+{
+	public static void main(String args[]) //main 
+	{
+		System.out.println("Hello world!"); //printing Hello world!
+		
+	}
+}
