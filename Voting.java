9e3cb40ad79178beb4857d1161bf758a19ed9282
Reddy Uma Devi <121800119+umareddy12@users.noreply.github.com>
2023-08-27 17:38:31 +0530

    Add files via upload

---
+++ Voting.java
@@ -0,0 +1,17 @@
+//Voting
+public class Voting
+{
+	public static void main(String args[])
+	{
+		int myAge=22;
+		int votingage=18;
+		if(myAge>=votingage)
+		{
+			System.out.println("U r elegible to vote");
+		}
+		else
+		{
+		System.out.println("U r not eligible to vote");	
+		}
+	}
+}
