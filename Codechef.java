e33c9462e1e3ce84a4d56e1dbd4f2191636bb83e
Reddy Uma Devi <121800119+umareddy12@users.noreply.github.com>
2023-09-07 13:48:30 +0530

    Codechef.java

---
+++ Codechef.java
@@ -0,0 +1,12 @@
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		// your code goes here
+		int s = 14 ;
+        int area = s * s ;
+        int cost = area * 7 ;
+        System.out.println(area);
+        System.out.print(cost+ "$");
+	}
+}
