1dfe922bbee2b33f26f4769f4d4f0d3f56175691
Reddy Uma Devi <121800119+umareddy12@users.noreply.github.com>
2023-09-08 10:42:31 +0530

    StringOper.java

---
+++ StringOper.java
@@ -0,0 +1,10 @@
+class StringOper
+{
+	public static void main (String[] args)
+	{
+		String var = "String";
+        System.out.print(var.charAt(0));  // Print the first 3 characters
+        System.out.print(var.charAt(1));
+        System.out.print(var.charAt(2));
+	}
+}
