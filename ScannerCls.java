e221fa3a9f197a81f2dd767550674799b3345e15
Reddy Uma Devi <121800119+umareddy12@users.noreply.github.com>
2023-09-09 21:31:13 +0530

    ScannerCls.java

---
+++ ScannerCls.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class ScannerCls
+{
+	public static void main (String[] args)
+	{
+		// update your code below this line
+       
+        Scanner read=new Scanner(System.in);
+        int num=read.nextInt();
+        System.out.println(num * num);
+        
+        
+
+	}
+}
