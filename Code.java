0ac6ba22b1585be3a8d40f7021c7a6c64411af82
Reddy Uma Devi <121800119+umareddy12@users.noreply.github.com>
2023-09-10 22:33:05 +0530

    Code.java

---
+++ Code.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Code
+{
+	public static void main (String[] args)
+	{
+		// your code goes here
+		Scanner read = new Scanner(System.in);
+		String x = read.nextLine();
+		System.out.println("Hello " + x);
+	}
+}
