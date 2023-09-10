52f895798756c69e5223615c421b459274f13753
Reddy Uma Devi <121800119+umareddy12@users.noreply.github.com>
2023-09-10 22:36:51 +0530

    Problem1.java

---
+++ Problem1.java
@@ -0,0 +1,10 @@
+class Codechef {
+  public static void main(String[] args) {
+    Scanner read = new Scanner(System.in);
+    int a = read.nextInt();
+    int b = read.nextInt();
+    int c = a + 2;
+    int d = c + b;
+    System.out.println(d);
+  }
+}
