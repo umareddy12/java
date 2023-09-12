a02a3f594ce5f20215b1589a4d6bf2ebf0886e2f
Reddy Uma Devi <121800119+umareddy12@users.noreply.github.com>
2023-09-12 20:50:12 +0530

    Score.java

---
+++ Score.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+
+class Score
+{
+    public static void main (String[] args)
+    {
+        // your code goes here
+        int b = 23;
+        int r = 45;
+        if(r > b){
+            System.out.println("Rob scored higher marks than Bob.");
+        }
+        else if(r >=b){
+            System.out.println("Bob & Rob both scored the same");
+        }
+        
+        b = 15;
+        r = 15;
+        if(r > b){
+            System.out.println("Rob scored higher marks than Bob.");
+        }
+        else if(r >= b){
+            System.out.println("Bob & Rob both scored the same");
+        }
+        
+    }
+}
