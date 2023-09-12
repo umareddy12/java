78f9d3ec18321c98557fa8ca548daa0db76b582d
Reddy Uma Devi <121800119+umareddy12@users.noreply.github.com>
2023-09-12 18:42:40 +0530

    Conditions.java

---
+++ Conditions.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Conditions
+{
+	public static void main (String[] args)
+	{
+		// your code goes here
+		Scanner read = new Scanner(System.in);
+		int a = read.nextInt();
+		int b = read.nextInt();
+		if (a >b){
+		    System.out.print("Coding is Fun!");
+		}
+	}
+}
