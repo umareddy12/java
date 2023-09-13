bfef1dc217db7e5c0f6fdd8efff668a1d6221121
Reddy Uma Devi <121800119+umareddy12@users.noreply.github.com>
2023-09-13 20:56:51 +0530

    Array1.java

---
+++ Array1.java
@@ -0,0 +1,16 @@
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Array1
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int[] array={10,20,30,40,50,60};
+		System.out.println(array.length);
+	}
+}
