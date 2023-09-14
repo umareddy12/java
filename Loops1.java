6d8f9763551583eed4092375b8c993f8e81294d1
Reddy Uma Devi <121800119+umareddy12@users.noreply.github.com>
2023-09-14 18:45:06 +0530

    Loops1.java

---
+++ Loops1.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Loops1
+{
+	public static void main (String[] args)
+	{
+		Scanner read = new Scanner(System.in);
+		int num = read.nextInt();
+		int a = 0;
+		// Update your code below this line
+        for(a=0;a<num;a++)
+        {
+        System.out.println(a);
+
+
+		}
+	}
+}
