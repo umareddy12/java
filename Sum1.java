e5ce4f83e8954fadaa5c7cf2ccff1c03c48002bc
Reddy Uma Devi <121800119+umareddy12@users.noreply.github.com>
2023-09-25 21:26:24 +0530

    Sum1.java

---
+++ Sum1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Sum1
+{
+    public static void main (String[] args)
+    {
+        Scanner read = new Scanner(System.in);
+        int a = read.nextInt();
+	int b = read.nextInt();
+	int c = a + b;
+    	System.out.print(c);
+    }
+}
