666f45378840f13cc294613dbfb53aa5195f9886
Reddy Uma Devi <121800119+umareddy12@users.noreply.github.com>
2023-09-25 21:24:31 +0530

    Create Input1.java

---
+++ Input1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Input1
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s=new Scanner(System.in);
+		int num=s.nextInt();
+		
+		System.out.println(num);
+	}
+}
