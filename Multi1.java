f7a52c4402526dedc1a0f9433cbdc614073f7ea2
Reddy Uma Devi <121800119+umareddy12@users.noreply.github.com>
2023-09-28 21:33:33 +0530

    Multi1.java

---
+++ Multi1.java
@@ -0,0 +1,17 @@
+//Chef and Instant Noodles
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Multi1
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s=new Scanner(System.in);
+		int X=s.nextInt();
+		int Y=s.nextInt();
+		System.out.println(X*Y);
+	}
+}
