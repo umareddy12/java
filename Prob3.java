4c760dfd081b365c397ac044c81b8f50b38c01ed
Reddy Uma Devi <121800119+umareddy12@users.noreply.github.com>
2023-10-01 21:19:44 +0530

    Prob3.java

---
+++ Prob3.java
@@ -0,0 +1,20 @@
+//Chef and Brain Speed
+import java.util.Scanner;
+
+class Prob3
+
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        int X=s.nextInt();
+        int Y=s.nextInt();
+        if(X<Y)
+        {
+            System.out.println("YES");
+        }
+        else{
+             System.out.println("NO");
+        }
+    }
+  }
