99a84e3468e01b4cd41c8ba14d20e36b83a1ba48
Reddy Uma Devi <121800119+umareddy12@users.noreply.github.com>
2023-10-05 19:41:59 +0530

    Bspeed.java

---
+++ Bspeed.java
@@ -0,0 +1,21 @@
+//Chef and Brain Speed
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Bspeed
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s=new Scanner(System.in);
+		int X=s.nextInt();
+		int Y=s.nextInt();
+		if(X<Y)
+		{
+		    System.out.println("YES");
+		}
+		else{
+		    System.out.println("NO");
+		}
+	}
+}
