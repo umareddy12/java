ef46bb3fd9fd6226656e837a82b79145d83e0233
Reddy Uma Devi <121800119+umareddy12@users.noreply.github.com>
2023-10-10 21:14:54 +0530

    Prblms1.java

---
+++ Prblms1.java
@@ -0,0 +1,21 @@
+//Code chef:Water Consumption problem
+class Prblms1
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s=new Scanner(System.in);
+	
+		int t=s.nextInt();
+			for(int i=0;i<t;i++){
+		int X=s.nextInt();
+		if(X>=2000)
+		{
+		    System.out.println("YES");
+		}
+		else{
+		    System.out.println("NO");
+		}
+		}
+	}
+}
