059d30f56835e85d7dacd052ba583c4bffbf56b1
Reddy Uma Devi <121800119+umareddy12@users.noreply.github.com>
2023-10-11 21:00:34 +0530

    Prblms2.java

---
+++ Prblms2.java
@@ -0,0 +1,26 @@
+//Code Chef:Age Limit problem
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Prblms2
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int i,T,x,y,a;
+		T = sc.nextInt();
+		for(i=0;i<T;i++){
+		    x = sc.nextInt();
+		    y = sc.nextInt();
+		    a = sc.nextInt();
+		    if(a>=x && a<y)
+		        System.out.println("YES");
+		    else
+		        System.out.println("NO");
+		}
+	}
+}
