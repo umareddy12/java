7296e77ae4c3b2d8d654926f5a4d57e3dcc60beb
Reddy Uma Devi <121800119+umareddy12@users.noreply.github.com>
2023-10-11 21:05:44 +0530

    Classes1.java

---
+++ Classes1.java
@@ -0,0 +1,21 @@
+//Codfe Chef problem:Biryani classes
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class  Classes1
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+Scanner sc=new Scanner(System.in);
+int t=sc.nextInt();
+while(t-->0)
+{
+int x=sc.nextInt();
+int y=sc.nextInt();
+    System.out.println(x*y);
+}
+	}
+}
