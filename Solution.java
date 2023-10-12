450675d627da88a6a32d7fcbd6f02ce93c434c76
Reddy Uma Devi <121800119+umareddy12@users.noreply.github.com>
2023-10-12 23:02:29 +0530

    Solution.java

---
+++ Solution.java
@@ -0,0 +1,17 @@
+// Hacker Rank:Java Stdin and Stdout I
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+       int x=scan.nextInt();
+       int y=scan.nextInt();
+        
+        System.out.println(a);
+        System.out.println(x);
+        System.out.println(y);
+        
+    }
+}
