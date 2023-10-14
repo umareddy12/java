79f12a61157c6d753d9b0106c4a0a873db23ce24
Reddy Uma Devi <121800119+umareddy12@users.noreply.github.com>
2023-10-14 20:56:01 +0530

    Welcome.java

---
+++ Welcome.java
@@ -0,0 +1,9 @@
+// Hacker Rank problem:Welcome to Java!
+public class Welcome {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
+        System.out.println("Hello, World.");
+System.out.println("Hello, Java.");
+    }
+}
