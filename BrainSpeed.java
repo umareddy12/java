f983c9f9cb021e958036be7bf4069ead0bd1f4af
Reddy Uma Devi <121800119+umareddy12@users.noreply.github.com>
2023-10-15 16:58:29 +0530

    BrainSpeed.java

---
+++ BrainSpeed.java
@@ -0,0 +1,25 @@
+//codechef problem:Chef and Brain Speed
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class BrainSpeed
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		
+	        int X = sc.nextInt(), Y = sc.nextInt();
+	        
+	   
+	        if(X<Y){
+	            System.out.println("Yes");
+	        }
+	        else{
+	            System.out.println("No");
+	        }
+	        
+	}
+}
