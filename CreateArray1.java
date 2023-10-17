9def4d0ea64c76a0698f09880d467ea3e394f284
Reddy Uma Devi <121800119+umareddy12@users.noreply.github.com>
2023-10-17 22:30:15 +0530

    CreateArray1.java

---
+++ CreateArray1.java
@@ -0,0 +1,12 @@
+//code chef problem:Creating Arrays
+
+class CreateArray1
+{
+	public static void main (String[] args)
+	{
+		// your code goes here
+		int[] num = {1, 2, 3, 4, 5};
+		System.out.print("Done");
+		
+	}
+}
