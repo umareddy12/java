49495b6c275020b53780e8b2235899c9bfa2af10
Reddy Uma Devi <121800119+umareddy12@users.noreply.github.com>
2023-10-18 19:19:10 +0530

    FuncnPrblm.java

---
+++ FuncnPrblm.java
@@ -0,0 +1,36 @@
+// codechef problem:Functions in java
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class FuncnPrblm
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner scanner = new Scanner(System.in);
+	    int A, B, C, D;
+        A = scanner.nextInt();
+        B = scanner.nextInt();
+        C = A * A + 2 * A * B + B * B;
+        D = A + B;
+        System.out.println(C);
+        System.out.println(D);
+
+        A = scanner.nextInt();
+        B = scanner.nextInt();
+        C = A * A + 2 * A * B + B * B;
+        D = A + B;
+        System.out.println(C);
+        System.out.println(D);
+
+        A = scanner.nextInt();
+        B = scanner.nextInt();
+        C = A * A + 2 * A * B + B * B;
+        D = A + B;
+        System.out.println(C);
+        System.out.println(D);
+
+        scanner.close();
+	}
+}
