d16989973f2133a958cd8550b29513a69eac8eee
Reddy Uma Devi <121800119+umareddy12@users.noreply.github.com>
2023-10-20 09:34:51 +0530

    Opfunction.java

---
+++ Opfunction.java
@@ -0,0 +1,12 @@
+//Printing Output from Functions:code chef problem
+public Opfunction  {
+    public static void main(String[] args) {
+        printSquare(5); // Output: Square of 5 is: 25
+    }
+
+    // User-defined method to print the square of a number
+    public static void printSquare(int num) {
+        int squareResult = num * num;
+        System.out.println("Square of " + num + " is: " + squareResult);
+    }
+}
