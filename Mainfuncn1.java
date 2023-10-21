33388b1becb54835d1d6607ac820548652319acc
Reddy Uma Devi <121800119+umareddy12@users.noreply.github.com>
2023-10-21 19:21:47 +0530

    Mainfuncn1.java

---
+++ Mainfuncn1.java
@@ -0,0 +1,14 @@
+//Codechef problem
+public class  Mainfuncn1 {
+    public static void main(String[] args) {
+        int A = 5;
+        int B = 3;
+        int result = addNumbers(A, B);
+        System.out.println("Sum: " + result);
+    }
+
+    // User-defined method to add two numbers
+    public static int addNumbers(int a, int b) {
+        return a + b;
+    }
+}
