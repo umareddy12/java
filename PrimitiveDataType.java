f943645c37e26640f07f5e453b74cfb5f9eaf5fd
Reddy Uma Devi <121800119+umareddy12@users.noreply.github.com>
2024-01-23 19:29:07 +0530

    Add files via upload

---
+++ PrimitiveDataType.java
@@ -0,0 +1,16 @@
+public class PrimitiveDataType{
+  public static void main(String args[])
+  {
+  byte b1=123;
+  short sh1=3456;
+  int num1=234554;
+  long l1=123456789;
+  float f1=78.83f;
+  double d1=981.23452345;
+  char ch1='A';
+  String str1="java";//this is not a primitive data type
+  boolean bl2=true;
+  System.out.println(b1 \n num1);	
+  }
+}
+
