b6c3aab5dbb5ba0dae79cd8f8d38c553d61d1339
Reddy Uma Devi <121800119+umareddy12@users.noreply.github.com>
2024-01-23 22:44:53 +0530

    Add files via upload

---
+++ GreatestNo.java
@@ -0,0 +1,16 @@
+//Greatest number
+public class GreatestNo{ 
+public static void main(String args[]){
+int n1=100,n2=12,n3=900;
+if(n1>=n2 &&n1>=n3){
+ System.out.println(n1);
+}
+else if(n2>=n1 && n2>=n3){
+System.out.println(n2);	
+}
+else{
+ System.out.println(n3);
+}
+ 
+}
+}
